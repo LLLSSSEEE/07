4a178414a2737188db0bdae07c07feaff22b964b
Chamomile <dltldms5271@gmail.com>
2023-10-19 16:12:40 +0900

    05

--- main.c
+++ main.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void f(void);
-int i;
+void sub(void);
 
 int main(int argc, char *argv[])
 {
-    for (i=0; i<5; i++)
-    {
-        f();
-    }
-    
+    int i;
+    for(i=0; i<3; i++)
+       sub();
+
     system("PAUSE");
     return 0;
 }
 
-void f(void)
+void sub(void)
 {
-     for (i=0; i<10; i++)
-        printf("#");
-
+     int auto_count = 0;
+     static int static_count = 0;
+     auto_count++;
+     static_count++;
+     printf("auto_count = %d\n", auto_count);
+     printf("static_count = %d\n", static_count);
+}
     
 //    system("PAUSE"); // 얘 안쓰면 검정색창 금방 사라짐 
 //    return 0;
-}
+
 
 
              
