a9243f2dce43203f8f8630cca60ab135a9a2d025
Chamomile <dltldms5271@gmail.com>
2023-10-19 16:22:32 +0900

    06-1

--- main.c
+++ main.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sub(void);
+int all_files;
 
-int main(int argc, char *argv[])
+extern void sub();
+
+int main(void)
 {
-    int i;
-    for(i=0; i<3; i++)
-       sub();
-
+    sub();
+    printf("%d\n", all_files);
+    
     system("PAUSE");
     return 0;
 }
 
-void sub(void)
-{
-     int auto_count = 0;
-     static int static_count = 0;
-     auto_count++;
-     static_count++;
-     printf("auto_count = %d\n", auto_count);
-     printf("static_count = %d\n", static_count);
-}
-    
+
+//int main(int argc, char *argv[])
+
 //    system("PAUSE"); // 얘 안쓰면 검정색창 금방 사라짐 
 //    return 0;
-
-
-
-             
-  
-  //system("PAUSE");	
-  //return 0; }
-//}
